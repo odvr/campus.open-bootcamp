709c8715872c18cfefde43b3302d78553c8f50c0
David Vega <omar.vegar@cun.edu.co>
2022-11-17 15:58:11 -0500

    JS_E1

---
+++ Curso JS/JS_E1.js
@@ -0,0 +1,11 @@
+let nombre = "Omar David Vega "
+let edad = 24
+let desarrollador = true
+let Fecha_nacimiento = "21/09/1998"
+
+const Libro_Favorito ={ titulo : "Ikigai", autor:" Hector Garcia, Francesc Mirallesr",fecha:'April 2016',url :'https://www.audible.com/pd/Ikigai-Los-secretos-de-Japon-para-una-vida-larga-y-feliz-Ikigai-Japanese-Secrets-for-a-Long-and-Happy-Life-Audiobook/B0797XWYVX?source_code=UHSPP30DTRIAL548101222017X&ipRedirectOverride=true&&cvosrc=ppc%20content.googlegdn.18595012441&cvo_crid=628101289156&cvo_pid=141954718986&source_code=&gclid=Cj0KCQiA1NebBhDDARIsAANiDD3DKeotH18d4BDVlvi6hWXNFnWY1vsFlwhXGETGzg8cBKf2MxhFedkaAojcEALw_wcB&gclsrc=aw.ds'
+
+
+}
+
+console.log(Libro_Favorito.autor)
