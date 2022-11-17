a1a3b629ac8c8ae4ea440732028d9947911d28c6
David Vega <omar.vegar@cun.edu.co>
2022-11-17 17:32:26 -0500

    JS3

---
+++ Curso JS/E3_E_Control.js
@@ -0,0 +1,20 @@
+let factorial = 1
+let num = 10
+while (num > 1) {
+    factorial *= num
+    num--
+}
+console.log(factorial) 
+let factorial2 = 1
+for (let i = 10; i > 0; i--) {
+    factorial *= i
+}
+console.log(factorial) 
+
+let factorial3 = 1
+let num1 = 10
+while (num > 1) {
+    factorial *= num
+    num--
+}
+console.log(factorial)
